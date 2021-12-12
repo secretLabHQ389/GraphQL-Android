87075056e00b4b203c44664036e0f626b420fa2f
GeorgeP225 <gpayne@greeningaustralia.org.au>
2021-12-12 20:00:20 +1100

    Apollo React client

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 // /http://localhost:4000/graphql
 
+//https://www.apollographql.com/docs/android/tutorial/01-configure-project/
+
 const express = require('express')
 const graphqlHTTP = require('express-graphql').graphqlHTTP
 //const { graphqlHTTP } = require('express-graphql')
@@ -12,14 +14,18 @@ const graphqlHTTP = require('express-graphql').graphqlHTTP
 //     console.log('DB connected')
 // })
 
+const cors = require('cors')
+const port = process.env.PORT || 4000
+
 const schema = require('./schema/schema')
 
 const app = express()
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
     schema
 }))
 
-app.listen(4000, () => {
+app.listen(port, () => {
     console.log('Having fun on port 4000')
 })
